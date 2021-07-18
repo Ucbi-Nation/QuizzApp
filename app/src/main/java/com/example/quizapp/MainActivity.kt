8a89bb96ce8fc1c775848798a839b9a9e9d25b74
df <>
2021-07-18 21:50:45 +0100

    A quizz app with kotlin

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener {
+
+            if (et_name.text.toString().isEmpty()) {
+
+                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+                // TODO (STEP 2: Pass the name through intent using the constant variable which we have created.)
+                // START
+                intent.putExtra(Constants.USER_NAME, et_name.text.toString())
+                // END
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+
+}
